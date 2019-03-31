f08abe9a546ace372ff92eb8dcae0ce6c9904859
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-03-31 13:19:48 -0700

    implement routing on edit and new feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { DropdownDirective  } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping.list.service';
 import { AppRoutingModule } from './app.routing.module';
 import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.compone
     RecipeDetailComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent 
+    RecipeStartComponent,
+    RecipeEditComponent 
   ],
   imports: [
     BrowserModule,
--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,6 +14,7 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
+    private router: Router,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -30,4 +31,9 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    //or:
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
+  }
 }
