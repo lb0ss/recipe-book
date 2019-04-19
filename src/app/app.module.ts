3aa03ea13a6f75681dc63474a6267f3babbcc8e6
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-04-18 20:12:10 -0700

    implement reactive form for recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -30,11 +30,12 @@ import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component'
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent 
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,8 +12,11 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params
@@ -19,9 +25,56 @@ export class RecipeEditComponent implements OnInit {
           this.id = +params['id'];
           // id is not null when editMode is true, otherwise editMode is false
           this.editMode = params['id'] != null;
+          this.initForm();
           console.log(this.editMode);
         }
       )
   }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  onAddIngredient() {
+
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDesc = '';
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDesc = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDesc),
+      'ingredients': recipeIngredients
+    });
+  }
+
 
 }
