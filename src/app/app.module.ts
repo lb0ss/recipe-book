b2c909f092de390c8e753d4d06a9672c58027956
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-04-23 20:54:56 -0700

    provide recipeService to the whole app to fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ShoppingListService } from './shopping-list/shopping.list.service';
 import { AppRoutingModule } from './app.routing.module';
 import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipe/recipe.service';
 
 
 @NgModule({
@@ -39,7 +40,7 @@ import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component'
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -4,14 +4,13 @@ import { RecipeService } from './recipe.service';
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css'],
-  providers: [RecipeService]
+  styleUrls: ['./recipe.component.css']
 })
-export class RecipeComponent implements OnInit {  
+export class RecipeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
   }
 
-  
+
 }
