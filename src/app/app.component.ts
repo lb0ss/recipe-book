da8402c25bee93630548d39c61e5f00cf82a9f00
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-05-20 13:43:10 -0700

    integrated firebase SDK with the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 loadedFeature = 'recipes';
+
+constructor() {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCvhsAPcJsx7HlLVC1GCUMopNNZGzr6TkY',
+    authDomain: 'ng-recipe-book.firebaseapp.com'
+  });
+}
+
+ngOnInit() {
+
+}
 
 }
