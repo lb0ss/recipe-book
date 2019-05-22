75a2113e9ab3132129f599b347605a1730bb56c7
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-05-21 20:02:59 -0700

    create a separate feature module (w/ routing) for recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective  } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping.list.service';
 import { AppRoutingModule } from './app.routing.module';
-import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipe/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
+import { RecipesModule } from './recipe/recipes.module';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -29,23 +24,17 @@ import { AuthGuard } from './auth/auth-guard.service';
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
     ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    RecipesModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
