af8f47f6cafdc2a070cf038d01c9d79ddc82f00f
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-05-22 18:06:25 -0700

    create a separate module for auth and its corresponding routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,20 @@ import { ShoppingListService } from './shopping-list/shopping.list.service';
 import { AppRoutingModule } from './app.routing.module';
 import { RecipeService } from './recipe/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipe/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    SigninComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +29,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     RecipesModule,
     ShoppingListModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    AuthModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { SigninComponent } from "./auth/signin/signin.component";
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes: Routes = [
-		{ path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'shopping-list', component: ShoppingListComponent },
-    { path: 'signup', component: SignupComponent},
-    { path: 'signin', component: SigninComponent}
+		{ path: '', component: HomeComponent},
+    { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
