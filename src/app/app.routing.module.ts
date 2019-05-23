02a2b6d3d0247947ffb59713933f4d63e728edc9
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-05-23 13:35:30 -0700

    implement canLoad guard for lazy loading

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+// import { AuthGuard } from "./auth/auth-guard.service";  //temporarily disabled
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent},
--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from "@angular/router";
 import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
@@ -13,5 +13,15 @@ export class AuthGuard implements CanActivate {
     }
     return true;
   }
+
+  // canLoad guard for lazy loading - temporarily disabled
+
+  // canLoad(route: Route) {
+  //    if (!this.authService.isAuthenticated()) {
+  //     this.router.navigate(['auth/signin']);
+  //     return false;
+  //    }
+  //    return true;
+  // }
 
 }
